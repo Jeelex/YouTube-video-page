465086b1a45d84cf88bd0b29bd7e4efdef5a8a8b
Gogi <gogion@protonmail.com>
2021-03-23 02:18:07 +0200

    added aside divs

---
+++ index.js
@@ -0,0 +1,22 @@
+// "use strict";
+// const sidebar = document.getElementById("sidebar");
+// const sidebarVideoDiv = document.querySelector(".sidebar__video-div");
+// // let sidebarVideoImageSource = document.querySelector(".sidebar__video-img").src;
+
+// function addSidebarVideos(times) {
+// 	for (let i = 0; i < times; i++) {
+// 		const cloneSidebarVideoDiv = sidebarVideoDiv.cloneNode(true);
+// 		sidebar.append(cloneSidebarVideoDiv);
+// 	}
+// }
+// addSidebarVideos(3);
+
+// // function changeImgSrc(num) {
+// // 	const lastThumbnailImage = document.querySelector("#sidebar > div:last-child > img");
+// // 	lastThumbnailImage.src = `images/thumbnails/thumbnail(${num}).png`;
+// // 	// for (let i = 1; i <= 20; i++) {
+// // 	// lastThumbnailImage.src = `images/thumbnails/thumbnail(${num}).png`;
+// // 	// }
+// // }
+
+// // changeImgSrc();
